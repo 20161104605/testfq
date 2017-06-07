95051fff24c29aacd44526af7ec5b2d773e703b3
20161104605 <fq20161104605@admindeimac-33.local>
2017-06-07 09:26:06 +0800

    Initial Commit

---
+++ testfq/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testfq
+//
+//  Created by fuqiang on 17/6/7.
+//  Copyright © 2017年 20161104605. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
